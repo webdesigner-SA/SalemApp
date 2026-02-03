3d5c7d75d1a18124314de694cacd4c84779426c2
Sarah <salammari0044@stu.kau.edu.sa>
2026-02-03 19:14:07 +0300

    Set up tab navigation icon and color

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,45 @@
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
     // The Tabs component
-    <Tabs>
-      <Tabs.Screen name="account" options={{ title: 'الحساب' }} />
-      <Tabs.Screen name="home" options={{ title: 'الرئيسية', headerShown: false }} />
-      <Tabs.Screen name="settings" options={{ title: 'الإعدادات' }} />
+    <Tabs
+      screenOptions={{
+        tabBarItemStyle: { paddingVertical: 10 },
+        tabBarLabelStyle: { marginTop: 2, fontSize: 12, fontWeight: '600' },
+        tabBarActiveTintColor: 'rgba(78, 4, 109, 1)',
+        tabBarInactiveTintColor: 'rgba(113, 113, 113, 1)',
+      }}
+    >
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'الحساب',
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons size={17} name={focused ? 'person' : 'person-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: 'الرئيسية',
+          headerShown: false,
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons size={17} name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'الإعدادات',
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons size={17} name={focused ? 'settings' : 'settings-outline'} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
